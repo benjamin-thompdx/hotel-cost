f48a2eb5e804a42e968227dd99e20f0cedb7f5fa
Benjamin Thom <jaminthom@gmail.com>
2020-02-23 16:41:17 -0800

    refactor using the OR (||) operator

--- HotelCost.cs
+++ HotelCost.cs
@@ -4,10 +4,10 @@ class HotelCost
 {
   static void Main()
   {
-    Console.WriteLine("In what season are you booking your stay?");
+    Console.WriteLine("In what season are you booking a stay?");
     string season = Console.ReadLine();
 
-    Console.WriteLine("On the weekend or week night?");
+    Console.WriteLine("On the weekend or a weeknight?");
     string dayOfWeek = Console.ReadLine();
 
     bool summer = season == "summer";
@@ -17,15 +17,12 @@ class HotelCost
     {
       Console.WriteLine("Your stay is probably going to be pretty expensive. It's both peak travel season AND the weekend.");
     }
-    else if (summer)
+    else if (summer || weekend)
     {
       Console.WriteLine("Your stay might be more expensive than normal!");
     }
-    else if (weekend)
+    else
     {
-      Console.WriteLine("Your stay might be more expensive than normal!");
-    }
-    else {
       Console.WriteLine("Your stay might be expensive, but it could be worse!");
     }
   }
